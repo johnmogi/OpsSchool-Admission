b17abbb35266ff4a17f58470bea2676172dd3d5f
johnmogi <anguru@gmail.com>
2022-01-01 05:10:50 +0200

    ^6

--- materials/vagrant/fixScripts/exercise1-fix.sh
+++ materials/vagrant/fixScripts/exercise1-fix.sh
@@ -2,5 +2,6 @@
 #add fix to exercise1 here
 # route not installed on vag machine.
 sudo su
+apt update
 apt install net-tools ; route delete 208.86.224.90
 curl http://www.textfiles.com/art/bnbascii.txt
--- materials/vagrant/fixScripts/exercise2-fix.sh
+++ materials/vagrant/fixScripts/exercise2-fix.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #add fix to exercise2 here
+# curl http://www.ascii-art.de/ascii/ab/007.txt - will work in machine, not in script.
 sudo chmod +x /etc/hosts
 sudo sed -i '/ascii.art.de/d' /etc/hosts
-curl http://www.ascii-art.de/ascii/ab/007.txt
